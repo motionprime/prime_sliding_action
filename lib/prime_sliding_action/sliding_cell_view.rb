09472db05b5e6de9432211e4323d56d3284f326a
chitsung.lin <cactis.lin@gmail.com>
2014-05-21 12:30:02 +0800

    Update sliding_cell_view.rb
    bug fixed

--- lib/prime_sliding_action/sliding_cell_view.rb
+++ lib/prime_sliding_action/sliding_cell_view.rb
@@ -100,7 +100,7 @@ class SlidingCellView < MPCellWithSection
   def tap
     if @sliding_action_buttons_state == :hidden && @table
       cell_index = @table.indexPathForCell(self)
-      @table.delegate.tableView(@table, didSelectRowAtIndexPath: cell_index)
+      @table.delegate.tableView(cell_index)
     else
       hide_actions
     end
